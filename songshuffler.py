7407e804b497c9afed13c6bbc4ffc5c0ab083026
RodolfoFlores27 <85150161+RodolfoFlores27@users.noreply.github.com>
2021-09-06 14:17:51 +0800

    Create songshuffler.py

---
+++ songshuffler.py
@@ -0,0 +1,32 @@
+import os
+import random
+import eyed3
+
+#put directory where your songs are located
+directory = 'C:\\Users\\Some_User\\Desktop\\Songs'
+
+#puts all files in the directory in a list
+songlist = os.listdir(directory)
+
+#generate random numbers to pair with song titles
+def get_random_nums():
+    numlist = []
+    for num in range(1, len(songlist)+1):
+        mylist.append(num)
+
+    random.shuffle(mylist)
+    return numlist
+
+def rename_songs():
+    for num, song in zip(random_nums, songlist):
+        audio = eyed3.load('C:\\Users\\compa\\Some_User\\Songs\\' + song)
+        src = song
+        dst = str(num) + '_' + audio.tag.title + '.mp3'
+
+        # rename() function will
+        # rename all the files
+        os.rename(src, dst)
+
+if __name__ == "__main__":
+	random_nums = get_random_nums()
+	rename_songs()
